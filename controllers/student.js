1c0e87432a62e0bed24d75ae8bf978ca28e39f9b
meghaMS <meghana.suresh@tarento.com>
2020-06-23 12:47:18 +0530

    Test

--- controllers/student.js
+++ controllers/student.js
@@ -45,43 +45,43 @@ exports.updateStudents = function (req, res) {
 }
 
 exports.saveStudents = function (req, res) {
-    if (!req.body || !req.body.student || !req.body.student.student_code || !req.body.student.student_name || !req.body.student.school_code || !req.body.student.class_code) {
-        let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_MISSING_PARAMETERS, COMPONENT).getRspStatus()
-        return res.status(apistatus.http.status).json(apistatus);
-    }
-    let student = req.body.student
-    BaseModel.findByCondition(School, { school_code: student.school_code }, function (err, schools) {
-        if (err || !schools || schools.length == 0) {
-            let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SCHOOL_NOTFOUND, COMPONENT).getRspStatus()
-            return res.status(apistatus.http.status).json(apistatus);
-        }
+    // if (!req.body || !req.body.student || !req.body.student.student_code || !req.body.student.student_name || !req.body.student.school_code || !req.body.student.class_code) {
+    //     let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_MISSING_PARAMETERS, COMPONENT).getRspStatus()
+    //     return res.status(apistatus.http.status).json(apistatus);
+    // }
+    // let student = req.body.student
+    // BaseModel.findByCondition(School, { school_code: student.school_code }, function (err, schools) {
+    //     if (err || !schools || schools.length == 0) {
+    //         let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SCHOOL_NOTFOUND, COMPONENT).getRspStatus()
+    //         return res.status(apistatus.http.status).json(apistatus);
+    //     }
 
-        BaseModel.findByCondition(ClassDb, { class_code: student.class_code }, function (err, classes) {
-            if (err || !classes || classes.length == 0) {
-                console.log(err)
-                let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_CLASS_NOTFOUND, COMPONENT).getRspStatus()
-                return res.status(apistatus.http.status).json(apistatus);
-            }
-            // BaseModel.findByCondition(Student, { student_code: student.student_code, status: STATUS_ACTIVE }, function (err, studentdb) {
-            // if (studentdb && studentdb.length > 0) {
-            //     let apistatus = new APIStatus(StatusCode.ERR_DATA_EXIST, COMPONENT).getRspStatus()
-            //     return res.status(apistatus.http.status).json(apistatus);
-            // }
-            LOG.info('----------Inserting Student---------------')
-            student.status = STATUS_ACTIVE
-            student.created_on = new Date()
-            BaseModel.saveData(Student, [student], function (err, doc) {
-                if (err) {
-                    let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
-                    return res.status(apistatus.http.status).json(apistatus);
-                }
-                LOG.info('----------Inserted Student---------------')
+    //     BaseModel.findByCondition(ClassDb, { class_code: student.class_code }, function (err, classes) {
+    //         if (err || !classes || classes.length == 0) {
+    //             console.log(err)
+    //             let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_CLASS_NOTFOUND, COMPONENT).getRspStatus()
+    //             return res.status(apistatus.http.status).json(apistatus);
+    //         }
+    //         // BaseModel.findByCondition(Student, { student_code: student.student_code, status: STATUS_ACTIVE }, function (err, studentdb) {
+    //         // if (studentdb && studentdb.length > 0) {
+    //         //     let apistatus = new APIStatus(StatusCode.ERR_DATA_EXIST, COMPONENT).getRspStatus()
+    //         //     return res.status(apistatus.http.status).json(apistatus);
+    //         // }
+    //         LOG.info('----------Inserting Student---------------')
+    //         student.status = STATUS_ACTIVE
+    //         student.created_on = new Date()
+    //         BaseModel.saveData(Student, [student], function (err, doc) {
+    //             if (err) {
+    //                 let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+    //                 return res.status(apistatus.http.status).json(apistatus);
+    //             }
+    //             LOG.info('----------Inserted Student---------------')
 
                 let response = new Response(StatusCode.SUCCESS, COMPONENT).getRsp()
                 return res.status(response.http.status).json(response);
-            })
-            // })
-        })
+    //         })
+    //         // })
+    //     })
 
-    })
+    // })
 }
